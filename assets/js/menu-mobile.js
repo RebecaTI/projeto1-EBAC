a629b82bb3b31d786a4b208f4e7ccab3ede75229
Rebeca <mebuffbeka@gmail.com>
2024-06-04 13:01:45 -0300

    Mudando style atravez do js

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -6,9 +6,11 @@ function showMenu(){
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
         menuMobile.classList.style.display='none';
+        //Muda o display p none
     } else {
         menuMobile.classList.add('open');
         menuMobile.classList.style.display='block';
+        //Muda o display p none
     }
 }
 
